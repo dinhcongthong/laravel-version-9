ef277f0c3340559481e93b8b8e222a137f7a2039
Thomas <dcthong0810@gmail.com>
2022-04-04 14:39:13 +0700

    create conflic

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -24,5 +24,18 @@ class HomeController extends Controller
     public function index()
     {
         return view('home');
+
+         // dfasdfhasdkl/fjasdfafa
+        // sdfjasl;kfjasl;fasdf
+        // kjasdf;fjaslkfjads
+        // dfasdfhasdkl/fjasdfafa
+        // sdfjasl;kfjasl;fasdf
+        // kjasdf;fjaslkfjads// dfasdfhasdkl/fjasdfafa
+        // sdfjasl;kfjasl;fasdf
+        // kjasdf;fjaslkfjads// dfasdfhasdkl/fjasdfafa
+        // sdfjasl;kfjasl;fasdf
+        // kjasdf;fjaslkfjads// dfasdfhasdkl/fjasdfafa
+        // sdfjasl;kfjasl;fasdf
+        // kjasdf;fjaslkfjads
     }
 }
